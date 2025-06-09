15e2624c938b2f88c685fc7e8c8bcebf44bfc2ab
b05702117 <kevinkevin872@gmail.com>
2025-06-09 05:58:51 +0000

    fix: remove PreCopy stage

--- fawn_workspace/fawn_backend/src/stage.rs
+++ fawn_workspace/fawn_backend/src/stage.rs
@@ -2,7 +2,6 @@ use std::{fs, io::{Read, Write}, path::Path};
 
 #[derive(Clone, Copy, Debug)]
 pub enum Stage {
-    PreCopy,
     TempMember,
     Normal,
 }
@@ -12,9 +11,8 @@ impl Stage {
         let mut buf = [0u8; 1];
         match fs::File::open(&p).and_then(|mut f| f.read_exact(&mut buf)) {
             Ok(_) => match buf[0] {
-                0 => Ok(Stage::PreCopy),
+                0 => Ok(Stage::Normal),
                 1 => Ok(Stage::TempMember),
-                2 => Ok(Stage::Normal),
                 _ => Ok(Stage::Normal), // fallback/default
             },
             Err(_) => Ok(Stage::Normal), // default on first boot or missing file
@@ -23,9 +21,8 @@ impl Stage {
 
     pub fn store<P: AsRef<Path>>(&self, p: P) -> std::io::Result<()> {
         let val = match self {
-            Stage::PreCopy => 0u8,
+            Stage::Normal => 0u8,
             Stage::TempMember => 1u8,
-            Stage::Normal => 2u8,
         };
         let mut f = fs::OpenOptions::new()
             .create(true).write(true).truncate(true).open(p)?;
